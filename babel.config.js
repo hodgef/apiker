94d780eb1e24d33af3bb5aba762f1e1915adef5f
Francisco Hodge <hello@franciscohodge.com>
2021-11-17 21:45:34 -0800

    Add transform runtime

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = {
   presets: [["@babel/env"]],
   plugins: [
     ["@babel/plugin-proposal-class-properties"],
-    ["@babel/plugin-transform-typescript"]
+    ["@babel/plugin-transform-typescript"],
+    ["@babel/plugin-transform-runtime", {
+      regenerator: true
+    }]
   ]
 };
