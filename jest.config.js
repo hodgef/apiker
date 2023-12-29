f588c50d078b487c09734328dd1db2c8b203b2b3
Francisco Hodge <hello@franciscohodge.com>
2023-12-29 11:50:50 -0500

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,9 @@ module.exports = {
     },
     transformIgnorePatterns: [
         "node_modules/(?!cfw-*)"
-    ]
+    ],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/scripts/testMock.js",
+        "\\.(css|less)$": "<rootDir>/scripts/testMock.js"
+    }
 }
